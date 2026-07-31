f33e687090969381a25fb3aee43aae843087a3dc
Gopika Ravi <rgopika739@gmail.com>
2026-07-31 15:25:13 +0530

    Initial commit

--- mobile-app/services/firebase.js
+++ mobile-app/services/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage'; // New import
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCe1Hd_MkJ7RYTz1hvYIa01VtqiMMQMFXo",
-  authDomain: "greendeal-50a04.firebaseapp.com",
-  projectId: "greendeal-50a04",
-  storageBucket: "greendeal-50a04.firebasestorage.app",
-  messagingSenderId: "701726333859",
-  appId: "1:701726333859:web:4d38db7ebd2dee5a439816"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
